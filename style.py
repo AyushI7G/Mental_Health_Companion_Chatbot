ccd483af5f4dd40dd5a4dbab44630c377b68e88c
AyushI7G <ayushigupta1187@gmail.com>
2025-10-14 23:27:50 +0530

    Update style.py

--- style.py
+++ style.py
@@ -1,27 +1,39 @@
 def set_pastel_theme():
     pastel_css = """
     <style>
-        /* Background & cards */
+        /* Background & app text */
         .stApp {
             background-color: #fff8f0;
             color: #333333;
         }
-        .css-1d391kg {
-            background-color: #fdf6f0;
-        }
+
         /* Chat message bubbles */
         .stChatMessage > div {
             border-radius: 15px;
         }
+
+        /* Bot messages text */
+        .stChatMessageAssistant p {
+            color: black !important;
+        }
+
+        /* User messages text */
+        .stChatMessageUser p {
+            color: black !important;
+        }
+
         /* Sidebar */
         .css-1v3fvcr {
             background-color: #ffe8e0;
         }
-        /* Buttons & inputs */
+
+        /* Buttons */
         .stButton>button {
             background-color: #ffcccb;
             color: #333;
         }
+
+        /* Text input box */
         .stTextInput>div>input {
             background-color: #fff0f0;
             color: #333;
